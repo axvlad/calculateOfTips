fa3d803cdb80c0a47d09d1b9322d64c9df6d85b7
Vlad Burma <burmavburma@gmail.com>
2023-02-23 22:33:46 +0200

    Initial Commit

---
+++ CalculateCaffe/CalculateCaffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculateCaffeApp.swift
+//  CalculateCaffe
+//
+//  Created by Vlad on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculateCaffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
